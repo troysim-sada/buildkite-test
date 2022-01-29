7ad7a0cbc1b186a6fb6719fbb21f8cfd91a73d4e
troysim-sada <98623407+troysim-sada@users.noreply.github.com>
2022-01-28 17:45:12 -0800

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   vpc_name       = "buildkite-test"
   gcp_project    = "sada-troy-sim"
   gcp_region     = "us-central1"
-  network_subnet  = "192.16.8.1.1/24"
+  network_subnet  = "192.168.1.1/24"
   #gcp_auth_file = "" // uncomment this if you have the authorization file
 }
 
